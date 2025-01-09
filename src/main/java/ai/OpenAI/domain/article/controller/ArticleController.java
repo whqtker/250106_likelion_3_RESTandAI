1186e27635527f98efa08844dc1a0b37e0230318
whqtker <chosh2001@naver.com>
2025-01-09 16:45:12 +0900

    fix

--- src/main/java/ai/OpenAI/domain/article/controller/ArticleController.java
+++ src/main/java/ai/OpenAI/domain/article/controller/ArticleController.java
@@ -3,6 +3,7 @@ package ai.OpenAI.domain.article.controller;
 import ai.OpenAI.domain.article.entity.Article;
 import ai.OpenAI.domain.article.service.ArticleService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -15,23 +16,39 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/article")
 @RequiredArgsConstructor
+@Slf4j
 public class ArticleController {
     private final ArticleService articleService;
 
     @GetMapping("/list")
     public String list(
-            @RequestParam(value = "page", defaultValue = "0") int page,
+            @RequestParam(value = "kwType", defaultValue = "") List<String> kwTypes,
+            @RequestParam(defaultValue = "") String kw,
+            @RequestParam(defaultValue = "0") int page,
             Model model
     ) {
         List<Sort.Order> sorts = new ArrayList<>();
         sorts.add(Sort.Order.desc("id"));
         Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts));
+
+        Map<String, Boolean> kwTypesMap = kwTypes
+                .stream()
+                .collect(Collectors.toMap(
+                        kwType -> kwType,
+                        kwType -> true
+                ));
+
+        // log.debug("kwTypesMap: {}", kwTypesMap);
         Page<Article> itemsPage = articleService.search(pageable);
         model.addAttribute("itemsPage", itemsPage);
+        model.addAttribute("kwTypesMap", kwTypesMap);
+
         return "article/list";
     }
 }
