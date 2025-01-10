a84c3b9b5a587beb4385d50faf7a389df024c564
whqtker <chosh2001@naver.com>
2025-01-10 11:55:18 +0900

    feat: membercontroller 구현

--- src/main/java/ai/OpenAI/domain/member/controller/ApiV1MemberController.java
+++ src/main/java/ai/OpenAI/domain/member/controller/ApiV1MemberController.java
@@ -3,9 +3,39 @@ package ai.OpenAI.domain.member.controller;
 import ai.OpenAI.domain.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/api/v1/members")
 public class ApiV1MemberController {
     private final MemberService memberService;
+
+    @PostMapping("/signup")
+    public void signup() {
+        System.out.println("signup");
+    }
+
+    @PostMapping("/signout")
+    public void signout() {
+        System.out.println("signout");
+    }
+
+    @PostMapping("/login")
+    public void login() {
+        System.out.println("login");
+    }
+
+    @PostMapping("/logout")
+    public void logout() {
+        System.out.println("logout");
+    }
+
+    @GetMapping("/mypage")
+    public void mypage() {
+        System.out.println("mypage");
+    }
 }
