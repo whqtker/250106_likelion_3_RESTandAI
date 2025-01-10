2e4f2b2c563ea500f7e8f128abef97105f6068c9
whqtker <chosh2001@naver.com>
2025-01-10 10:07:15 +0900

    feat: implement article controller

---
+++ src/main/java/ai/OpenAI/domain/article/controller/ApiV1ArticleController.java
@@ -0,0 +1,43 @@
+package ai.OpenAI.domain.article.controller;
+
+import ai.OpenAI.domain.article.service.ArticleService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/articles")
+public class ApiV1ArticleController {
+    private final ArticleService articleService;
+
+    // Create
+    @PostMapping
+    public void postArticle() {
+        articleService.write(1L, "title", "content");
+    }
+
+
+    // Read
+    @GetMapping
+    public void getArticle() {
+        articleService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public void getArticles(@PathVariable("id") Long id) {
+        articleService.findById(id);
+
+    }
+
+    // Update
+    @PatchMapping("/{id}")
+    public void patchArticle(@PathVariable("id") Long id) {
+        articleService.modify(null, "title", "content");
+    }
+
+    // Delete
+    @DeleteMapping("/{id}")
+    public void deleteArticle(@PathVariable("id") Long id) {
+        articleService.findById(id);
+    }
+}
